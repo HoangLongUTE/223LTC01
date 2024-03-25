dbaa2b53b6b827baf0ef50135360f6b0c2f25e29
HoangLongUTE <21115053120123ute@gmail.com>
2024-03-25 13:39:52 +0700

    bai giua ki

---
+++ CaiHoangLong_120123_BTGK/CaiHoangLong_120123_BTGK/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaiHoangLong_120123_BTGK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
